814d526e5ad4bedfeee8fea65c3c2d14756dd2d6
Dddarknight <9801677@gmail.com>
2022-11-09 20:31:14 +0300

    change settings

--- workers_tree/settings.py
+++ workers_tree/settings.py
@@ -66,16 +66,24 @@ WSGI_APPLICATION = 'workers_tree.wsgi.application'
 
 DATABASE_URL = 'postgresql://<postgresql>'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    },
+SQLITE_SETTINGS = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
+
+if os.getenv('DB_ENGINE') == 'SQLite':
+    DATABASES = {'default': SQLITE_SETTINGS}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+            'USER': os.getenv('POSTGRES_USER', 'postgres'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+            'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        },
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
